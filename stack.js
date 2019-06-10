cd1df80037bdce33da054b210367ed596d34a666
Pallav Jain <pallavjain409@gmail.com>
2019-06-10 08:29:56 +0530

    stack constructor

--- stack.js
+++ stack.js
@@ -19,4 +19,34 @@ Stack.prototype.peek = function() {
 
 Stack.prototype.count = function() {
   // implement me...
-};
+};function Stack(capacity){
+    this._capacity = capacity;
+    this._storage = {};
+    this._count = 0;
+
+}
+Stack.prototype.push= function(val){
+    if (this._count < this._capacity){
+        this._storage[this._count++] = val;
+        return this._count;
+    }
+    return "Maximum Storage Reached";
+}
+ Stack.prototype.pop = function(){
+     if (this._count > 0){
+        var value = this._storage[--this._count];
+        delete this._storage[this._count];
+        return value;
+     }
+     return "empty"
+ }
+ Stack.prototype.count = function(){
+     return this._count;
+ }
+ Stack.prototype.peek = function(){
+     if (this._count > 0){
+     return this._storage[this._count-1];
+     }
+     return "empty";
+ }
+ var myStack = new Stack(3);
