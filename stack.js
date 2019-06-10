4a566c76f8bbebae6370cb6ce5922a4d08fcbb3c
Pallav Jain <pallavjain409@gmail.com>
2019-06-10 08:27:39 +0530

    stack constructor and functions

---
+++ stack.js
@@ -0,0 +1,22 @@
+function Stack(capacity) {
+  // implement me...
+}
+
+Stack.prototype.push = function(value) {
+  // implement me...
+};
+// Time complexity:
+
+Stack.prototype.pop = function() {
+  // implement me...
+};
+// Time complexity:
+
+Stack.prototype.peek = function() {
+  // implement me...
+};
+// Time complexity:
+
+Stack.prototype.count = function() {
+  // implement me...
+};
